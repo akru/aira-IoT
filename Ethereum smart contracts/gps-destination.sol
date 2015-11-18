4fb44e3e5492a422dc24c51c43ea8b3e462830ac
Sergey Lonshakov <sergeylonshakov@gmail.com>
2015-11-19 00:38:51 +0400

    Update gps-destination.sol
    add simple drone and customer functions

--- Ethereum smart contracts/gps-destination.sol
+++ Ethereum smart contracts/gps-destination.sol
@@ -1,18 +1,45 @@
 contract gpsdestination { 
-    string public longitude;
-	string public latitude;
+    address dronAccount;
+    address public currentCustomer;
+    string public currentLongitude;
+    string public currentLatitude;
+    string public destinationLongitude;
+    string public destinationLatitude;
 
-    event NewGPSCoordinates(string longitude_dest, string latitude_dest);
+
+    event NewGPSCoordinates(string destinationLongitude, string destinationLatitude);
+    event droneComeback(string currentLongitude, string currentLatitude);
 
     /*Initial */
-    function gpsdestination() {
+    function gpsdestination(string _currentLongitude, string _currentLatitude) {
+        dronAccount = msg.sender;
+        currentLongitude = _currentLongitude;
+        currentLatitude = _currentLatitude;
+    }
+    
+    /* Drone functions */
+        function homebase(string _currentLongitude, string _currentLatitude) returns(bool result) {
+        if(msg.sender==dronAccount) {
+        currentLongitude = _currentLongitude;
+        currentLatitude = _currentLatitude;
+        return true;}
     }
 
-    /* Founder functions */
-    function newGPSCoordinates(string _longitude_dest, string _latitude_dest) returns(bool result) {
-		longitude = _longitude_dest;
-		latitude = _latitude_dest;
-		NewGPSCoordinates(longitude, latitude);
-		return true;
+
+    /* Customer functions */
+
+    function getFlightCost(string _destinationLongitude, string _destinationLatitude) returns(uint flightCost) {
+        flightCost = 5;
+        return flightCost;
+    }
+
+    function takeFlight(string _destinationLongitude, string _destinationLatitude) returns(bool result) {
+        if(msg.value>=this.getFlightCost(_destinationLongitude, _destinationLatitude)) {
+        currentCustomer = msg.sender;
+        destinationLongitude = _destinationLongitude;
+        destinationLatitude = _destinationLatitude;
+        NewGPSCoordinates(destinationLongitude, destinationLatitude);
+        return true;
+        }
     }
 }
