21680918a5b53efb69732c6ae58843fb74c8ed99
Sergey Lonshakov <sergeylonshakov@gmail.com>
2015-11-20 16:36:32 +0400

    Add estimates

--- Ethereum smart contracts/gps-destination.sol
+++ Ethereum smart contracts/gps-destination.sol
@@ -5,39 +5,75 @@ contract gpsdestination {
     string public currentLatitude;
     string public destinationLongitude;
     string public destinationLatitude;
+    uint public estimatesActualBefore;
 
+    /* Estimates data */
+
+    mapping (address => uint) customerEstimatesOf;
+
+    struct Estimate {
+        address customerAddr;
+        string destinationLongitude;
+        string destinationLatitude;
+        uint cost;
+        uint actualBefore; 
+    }
+
+    Estimate[] public estimates; 
+
+    /* Events */
 
     event NewGPSCoordinates(string destinationLongitude, string destinationLatitude);
     event droneComeback(string currentLongitude, string currentLatitude);
+    event NewEstimate(uint estimateID);
 
     /*Initial */
-    function gpsdestination(string _currentLongitude, string _currentLatitude) {
+    function gpsdestination(string _currentLongitude, string _currentLatitude, uint _estimatesActualBefore) {
         dronAccount = msg.sender;
         currentLongitude = _currentLongitude;
         currentLatitude = _currentLatitude;
+        estimatesActualBefore = _estimatesActualBefore * 1 minutes;
     }
     
     /* Drone functions */
-        function homebase(string _currentLongitude, string _currentLatitude) returns(bool result) {
+    function homebase(string _currentLongitude, string _currentLatitude) returns(bool result) {
         if(msg.sender==dronAccount) {
         currentLongitude = _currentLongitude;
         currentLatitude = _currentLatitude;
         return true;}
     }
 
+    function setEstimateCost(uint _estimateID, uint _cost) returns(bool result) {
+        if(msg.sender==dronAccount) {
+            Estimate e = estimates[_estimateID];
+            e.cost = _cost;
+            return true;
+        }
+    }
+
 
     /* Customer functions */
 
-    function getFlightCost(string _destinationLongitude, string _destinationLatitude) returns(uint flightCost) {
-        flightCost = 5;
-        return flightCost;
+    function setNewEstimate(string _destinationLongitude, string _destinationLatitude) returns(uint estimateID) {
+        estimateID = estimates.length++;
+        Estimate e = estimates[estimateID];
+        e.customerAddr = msg.sender;
+        e.destinationLongitude = _destinationLongitude;
+        e.destinationLatitude = _destinationLatitude;
+        e.actualBefore = now + estimatesActualBefore;
+        customerEstimatesOf[msg.sender] = estimateID;
+        NewEstimate(estimateID);
+        return estimateID;
     }
 
-    function takeFlight(string _destinationLongitude, string _destinationLatitude) returns(bool result) {
-        if(msg.value>=this.getFlightCost(_destinationLongitude, _destinationLatitude)) {
+    function takeFlight() returns(bool result) {
+        uint workEstimateID;
+        workEstimateID = customerEstimatesOf[msg.sender];
+        Estimate e = estimates[workEstimateID];
+        if(msg.value>=e.cost) {
         currentCustomer = msg.sender;
-        destinationLongitude = _destinationLongitude;
-        destinationLatitude = _destinationLatitude;
+        destinationLongitude = e.destinationLongitude;
+        destinationLatitude = e.destinationLatitude;
         NewGPSCoordinates(destinationLongitude, destinationLatitude);
         return true;
         }
