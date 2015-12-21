eade27ac10ecbe9109321c7931e4b36cc75f11ce
Sergey Lonshakov <sergeylonshakovgmail.com>
2015-12-21 15:08:49 +0400

    add mothebase.sol

---
+++ Ethereum smart contracts/example/motherbase.sol
@@ -0,0 +1,34 @@
+contract motherbase {
+    address creator;
+    int256  public homebaseLongitude;
+    int256  public homebaseLatitude;
+    mapping (address => bool) droneActiveOf;
+    address public airwayAdminAddr;
+
+
+    modifier creatorCheck { if (msg.sender == creator) _ }
+
+    /* Functions */
+    function motherbase(int256 _homebaseLongitude,
+                        int256 _homebaseLatitude,
+                        address _airwayAdminAddr) {
+        creator = msg.sender;
+        homebaseLatitude = _homebaseLatitude;
+        homebaseLongitude = _homebaseLongitude;
+        airwayAdminAddr = _airwayAdminAddr;
+    }
+
+    function setDrone(address _droneAddr) creatorCheck returns(bool result) {
+        droneActiveOf[_droneAddr] = true;
+        result true;
+    }
+
+    function inactiveDrone(address _droneAddr) creatorCheck returns(bool result) {
+        droneActiveOf[_droneAddr] = false;
+        result true;
+    }
+
+    
+
+
+}
