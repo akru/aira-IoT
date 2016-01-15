02d721f8b28b808f82018636f26b6521bf982960
Alexander Krupenkin <mail@akru.me>
2016-01-16 02:05:21 +0300

    Added SmallATC AIRA ROS Bridge messages.

--- ROS/aira_ros_bridge/lib/msg/small_atc_msgs/RouteResponse.js
+++ ROS/aira_ros_bridge/lib/msg/small_atc_msgs/RouteResponse.js
@@ -1,7 +1,7 @@
 // Init libs
 var ROSLIB = require('roslib');
 var Helpers = require('../helpers');
-var SatFix = require('../dron_common_msgs/SatFix');
+var SatFix = require('../small_atc_msgs/SatFix');
 // Solidity message definition
 var bytecode = '606060405260405161019038038061019083398101604052805160805160a051919290910160a05160008054610100850264ffffffff001960ff1990921687179190911617815582516001805482825592819052927fb10e2d527612073b26eecdfd717e6a320cf44b4afac2b0732d9fcbe2b7fa0cf69283019290608001821560c2579160200282015b8281111560c25782518254600160a060020a03191617825560209290920191908401906089565b505050505060a8806100e86000396000f35b5060b09291505b8082111560e4578054600160a060020a0319168155830160c9565b509056606060405260e060020a6000350463af640d0f8114602e578063b980c78f146042578063c1991219146093575b005b609e60005463ffffffff6101009091041681565b609e600435600180548290811015600257506000527fb10e2d527612073b26eecdfd717e6a320cf44b4afac2b0732d9fcbe2b7fa0cf6015473ffffffffffffffffffffffffffffffffffffffff1681565b609e60005460ff1681565b6060908152602090f3';
 var abi = [{"constant":true,"inputs":[],"name":"id","outputs":[{"name":"","type":"uint32"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"route","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"valid","outputs":[{"name":"","type":"bool"}],"type":"function"},{"inputs":[{"name":"_valid","type":"bool"},{"name":"_id","type":"uint32"},{"name":"_route","type":"address[]"}],"type":"constructor"}];
